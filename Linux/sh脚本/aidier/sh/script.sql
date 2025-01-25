5e8ace0e97a69c2e63c7f816df0b7218ce108236
yxx7318 <1303490776@qq.com>
2025-01-25 17:21:57 +0800

    修正数据库自动安装脚本错误

--- Linux/sh脚本/aidier/sh/script.sql
+++ Linux/sh脚本/aidier/sh/script.sql
@@ -7,15 +7,21 @@
 -- SET GLOBAL validate_password.policy=LOW;
 -- SET GLOBAL validate_password.length=1;
 
+
 -- 更改root用户的密码，并将其设置为永不过期
-alter user 'root'@'localhost' identified by 'yxx@12345678&mysql' password expire never;
+-- alter user 'root'@'localhost' identified by 'yxx@12345678&mysql' password expire never;
 
 -- 更改root密码并明确指定使用mysql_native_password认证插件
-alter user 'root'@'localhost' identified with mysql_native_password by 'yxx@12345678&mysql';
+-- alter user 'root'@'localhost' identified with mysql_native_password by 'yxx@12345678&mysql';
+
+-- 合并上述两条命令
+ALTER USER 'root'@'localhost' 
+IDENTIFIED WITH mysql_native_password BY 'yxx@12345678&mysql'
+PASSWORD EXPIRE NEVER;
 
 flush privileges;
 
-use mysql
+use mysql;
 
 -- 将用户名为root且主机名为localhost的记录的host字段改为%,允许root用户从任何主机连接到MySQL服务器
 update user set host='%' where user='root' and host='localhost';
